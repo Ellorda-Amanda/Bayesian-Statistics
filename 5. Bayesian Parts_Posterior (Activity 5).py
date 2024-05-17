91bd804b9c0dee5baa021252bf5f6229dc6601d7
Ellorda-Amanda <amandaisabelellorda@gmail.com>
2024-05-17 17:55:32 +0800

    Add files via upload

---
+++ 5. Bayesian Parts_Posterior (Activity 5).py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar  9 20:46:34 2024
+
+@author: Abalora, Dellavan, Ellorda, Esclamado
+"""
+
+import scipy.stats as sts
+import numpy as np
+import matplotlib.pyplot as plt
+
+mu = np.linspace(1.65, 1.8, num = 50)
+
+uniform_dist = sts.uniform.pdf(mu) + 1 
+
+uniform_dist = uniform_dist/uniform_dist.sum()
+
+def likelihood_func(datum, mu):
+ likelihood_out = sts.norm.pdf(datum, mu, scale = 0.1)    
+ return likelihood_out/likelihood_out.sum() 
+
+likelihood_out = likelihood_func(1.7, mu)
+
+unnormalized_posterior = likelihood_out * uniform_dist
+plt.plot(mu, unnormalized_posterior)
+plt.xlabel("$\mu$ in meters")
+plt.ylabel("Unnormalized Posterior")
+plt.show()
